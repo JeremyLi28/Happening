12601d8f74aba0e1a76969e40ec49e40680fe26f
JeremyLi28 <270768253@qq.com>
2016-05-31 21:05:18 -0700

    add basic front-end

--- Happening/public/javascripts/hello.js
+++ Happening/public/javascripts/hello.js
@@ -1,3 +1,44 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+var app = angular.module("Happening", ['leaflet-directive']);
+
+app.factory('Twitter', function($http) {
+
+  var ws = new WebSocket("ws://localhost:9000/ws");
+
+  var twitterService = {
+    tweets: [],
+    query: function (query) {
+      ws.send(JSON.stringify({query: query}));
+    }
+  };
+
+  ws.onmessage = function(event) {
+    $timeout(function() {
+      twitterService.tweets = JSON.parse(event.data).statuses;
+    });
+  };
+  
+  return twitterService;
+});
+
+
+app.controller("AppCtrl", function ($scope, leafletData, Twitter) {
+
+  $scope.init = function() {
+    leafletData.getMap().then(function (map) {
+      $scope.map = map;
+      map.setView([33.643175, -117.841176], 15);
+    })
+  };
+
+  $scope.tweets = [];
+
+  $scope.$watch(
+    function() {
+      return Twitter.tweets;
+    },
+    function(tweets) {
+      $scope.tweets = tweets;
+    }
+  );
+
+});
